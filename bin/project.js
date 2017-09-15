7ab14002bbc097fcd760f9e99678122d2f0a5e4e
Andifeind <andifeind@noname-media.com>
2017-09-15 08:30:46 +0200

    project cmd test

--- bin/project.js
+++ bin/project.js
@@ -4,8 +4,9 @@ const ProjectManager = require('../src/utils/ProjectManager')
 const colorfy = require('colorfy')
 
 module.exports = (supershit) => {
-  supershit
+  return supershit
     .cmd('project [name] [projectDir]')
+    .option('-o, --overwrite', 'Overwrite existing files')
     .action((ctx, projectName, projectDir) => {
       projectName = ProjectManager.getProjectName(projectName)
       projectDir = ProjectManager.getProjectDir(projectName, projectDir)
@@ -18,6 +19,10 @@ module.exports = (supershit) => {
           const name = fl.isDir ? `${fl.relative}/` : fl.relative
           const type = fl.isDir ? 'dir ' : 'file'
           cf.txt(` create ${type}`).grey(name).nl()
+
+          if (fl.fileExists) {
+            cf.orange(fl.fileOverwritted ? '(replaced)' : '(skipped)')
+          }
         }
 
         cf.green('DONE').print()
---
+++ tests/integration/cli/project.spec.js
@@ -0,0 +1,86 @@
+const path = require('path')
+
+const inspect = require('inspect.js')
+const sinon = require('sinon')
+inspect.useSinon(sinon)
+
+const SuperFS = require('superfs')
+
+const shellInspect = require('shell-inspect')
+const CMD = path.join(__dirname, '../../../bin/supershit')
+
+const PROJECT_DIR = path.join(__dirname, '../../tmp/')
+
+describe.only('CLI', () => {
+  before((done) => {
+    SuperFS.deleteDir(PROJECT_DIR).then(() => done()).catch(() => {
+      done()
+    })
+  })
+
+  describe('project command', () => {
+    it('should output a project page', () => {
+      return shellInspect
+        .cmd(`${CMD} project test ${PROJECT_DIR}`)
+        .test((ctx) => {
+          inspect(ctx.exitCode).isEql(0)
+          inspect(ctx.text).doesContain('DONE')
+        })
+    })
+
+    it('should write a package.json file', () => {
+      const pkgFile = path.join(`${PROJECT_DIR}`, 'package.json')
+      inspect(pkgFile).isFile()
+
+      const pkg = require(pkgFile)
+      inspect(pkg).hasProps({
+        name: 'test'
+      })
+    })
+
+    const FILES = [
+      'app/app.js',
+      'web/web.js'
+    ]
+
+    FILES.forEach((file) => {
+      it(`should create a ${file} file`, () => {
+        inspect(path.join(PROJECT_DIR, file)).isFile()
+      })
+    })
+
+    it('should not overwrite existing files', () => {
+      return shellInspect
+        .cmd(`${CMD} project foo ${PROJECT_DIR}`)
+        .test((ctx) => {
+          inspect(ctx.exitCode).isEql(0)
+          inspect(ctx.text).doesContain('DONE')
+
+          const pkgFile = path.join(`${PROJECT_DIR}`, 'package.json')
+          inspect(pkgFile).isFile()
+
+          const pkg = require(pkgFile)
+          inspect(pkg).hasProps({
+            name: 'test'
+          })
+        })
+    })
+
+    it('should overwrite existing files if -o flag was set', () => {
+      return shellInspect
+        .cmd(`${CMD} project -o foo ${PROJECT_DIR}`)
+        .test((ctx) => {
+          inspect(ctx.exitCode).isEql(0)
+          inspect(ctx.text).doesContain('DONE')
+
+          const pkgFile = path.join(`${PROJECT_DIR}`, 'package.json')
+          inspect(pkgFile).isFile()
+
+          const pkg = require(pkgFile)
+          inspect(pkg).hasProps({
+            name: 'foo'
+          })
+        })
+    })
+  })
+})
