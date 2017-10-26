e973193e5ac8c5f3870ffb51f96cfe4825ecd880
Andi Heinkelein <andreas.heinkelein@maxdome.de>
2017-10-26 19:32:45 +0200

    restart cmd

--- bin/restart.js
+++ bin/restart.js
@@ -4,18 +4,10 @@ const path = require('path')
 const pm2 = require('pm2')
 const CLITools = require('../src/utils/CLITools')
 
-/**
- * Starts the app
- *
- * @cmd start
- * @example shell
- * supershit start
- */
 module.exports = (supershit) => {
   return supershit
-    .cmd('start')
-    .description('Starts a supershit app')
-    .option('-i, --instances <number>', 'Start [number] instances')
+    .cmd('stop')
+    .description('Stopss a running supershit app')
     .action((ctx) => {
       pm2.connect((err) => {
         if (err) {
@@ -23,12 +15,7 @@ module.exports = (supershit) => {
           process.exit(1)
         }
 
-        pm2.start({
-          script: path.join(process.cwd(), 'app/app.js'),
-          exec_mode: 'cluster',
-          instances: ctx.instances || 4,
-          max_memory_restart: '100M'
-        }, (err, apps) => {
+        pm2.restart(path.join(process.cwd(), 'app/app.js'), (err, apps) => {
           pm2.list((err, list) => {
             if (err) {
               throw err
